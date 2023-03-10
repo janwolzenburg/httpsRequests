94ee8d1c637dc0cfcadf609005cc189328232d5a
janwolzenburg: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 <72450694+janwolzenburg@users.noreply.github.com>
2023-03-10 16:45:29 +0100

    Added json request example

---
+++ examples/jsonRequest.cpp
@@ -0,0 +1,27 @@
+#include <string>
+using std::string;
+
+#include <iostream>
+
+#include "httpsRequests.h"
+
+
+int main( int argc, char* argv[] ) {
+
+    // API URL
+    const string url{ "https://app.divera247.com/api/v2/auth/login" };
+
+    // Only accept json responens
+    const vector<string> header{ "accept: application/json",
+								 "Content-Type: application/json" };
+
+    // Request data
+    const string postData{ "{ \"Login\": { \"username\": \"heinz.meinz@musterstadt.de\", \"password\": \"string\", \"jwt\": false } }" };
+
+    
+    httpsRequest req{ url, header, postData, httpsRequest::SSL_TYPE::NONE };
+
+    std::cout << req.getResponseString();
+
+    return 0;
+}
